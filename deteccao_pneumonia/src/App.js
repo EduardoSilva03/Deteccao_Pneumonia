3b800884cea3f9792a44db380741abdda67e233e
Leonardo <leonardoreitz0@gmail.com>
2025-06-24 19:29:29 -0300

    Rota de acesso

--- deteccao_pneumonia/src/App.js
+++ deteccao_pneumonia/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Acessar from './Components/Acessar/acessar';
 import Registrar from './Components/Registrar/registrar';
 import Anexos from './Components/Anexos/anexos';
-
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/acessar" />} />
         <Route path="/acessar" element={<Acessar />} />
         <Route path="/registrar" element={<Registrar />} />
         <Route path="/anexos" element={<Anexos />} />
